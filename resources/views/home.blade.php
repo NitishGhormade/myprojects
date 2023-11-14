bd0e105209b8aabeb9d44009a4b727d25fddd7ef
Nitish Ghormade <dadughormade@gmail.com>
2023-11-14 17:09:57 +0530

    home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Personality Exploration</title>
+    <style>
+        body {
+    font-family: 'Arial', sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #f7f7f7;
+    color: #333;
+}
+
+header {
+    background-color: #3498db;
+    color: #fff;
+    text-align: center;
+    padding: 1em 0;
+}
+
+.container {
+    width: 80%;
+    margin: 0 auto;
+}
+
+.intro {
+    padding: 2em 0;
+}
+
+.image-gallery {
+    background-color: #ecf0f1;
+    padding: 2em 0;
+}
+
+.image-container {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 1em;
+}
+
+img {
+    max-width: 30%;
+    border-radius: 8px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+    </style>
+</head>
+<body>
+
+    <header>
+        <h1>Discover Your Personality</h1>
+        <p>Explore the depths of who you are</p>
+    </header>
+
+    <section class="intro">
+        <div class="container">
+            <h2>What is Personality?</h2>
+            <p>Personality refers to individual differences in characteristic patterns of thinking, feeling, and behaving. It encompasses a wide range of traits and behaviors that make each person unique.</p>
+        </div>
+    </section>
+
+    <section class="image-gallery">
+        <div class="container">
+            <h2>Visualizing Personality</h2>
+            <div class="image-container">
+                <img src="https://img.freepik.com/free-vector/boy-with-red-nose-blue-background_1308-92638.jpg?size=626&ext=jpg&ga=GA1.1.386372595.1698019200&semt=ais" alt="Personality Image 1">
+                <img src="https://as1.ftcdn.net/v2/jpg/01/77/51/90/1000_F_177519026_R01iez2UYKZW4frdE6W5yHiEnnCBz6NH.jpg" alt="Personality Image 2">
+                <img src="https://media.istockphoto.com/id/1323019807/vector/cartoon-boss-man-greedy-vector-illustration.jpg?s=612x612&w=0&k=20&c=3lMisEVaono2DgsmfZK8q__e8EBZGruS8dxwwDkSK8Y=" alt="Personality Image 3">
+            </div>
+        </div>
+    </section>
+
+</body>
+</html>
